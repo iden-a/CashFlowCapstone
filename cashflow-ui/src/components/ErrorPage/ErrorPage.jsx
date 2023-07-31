408bf449c47f19d633e425fcf14dc700968f9ad8
Iden Amoako <iamoako@iamoako-ltmuqpv.internal.salesforce.com>
2023-07-31 11:37:55 -0700

    errorlink for dark/light mode

--- cashflow-ui/src/components/ErrorPage/ErrorPage.jsx
+++ cashflow-ui/src/components/ErrorPage/ErrorPage.jsx
@@ -1,24 +1,24 @@
 import React, { Fragment, useState } from "react";
 
-export default function ErrorPage() {
+export default function ErrorPage({errorLink}) {
   return (
     <Fragment>
       <div className="stack" style={{"--stacks" : "3"}}>
         <img
           id="404"
-          src="404light.png"
+          src={errorLink}
           alt="image of Cashflow Academy's slogan"
           style={{ width: "30%", height: "70%", margin: "0 auto", "--index" : "0" }}
         />
         <img
           id="404"
-          src="404light.png"
+          src={errorLink}
           alt="image of Cashflow Academy's slogan"
           style={{ width: "30%", height: "70%", margin: "0 auto", "--index" : "1" }}
         />
         <img
           id="404"
-          src="404light.png"
+          src={errorLink}
           alt="image of Cashflow Academy's slogan"
           style={{ width: "30%", height: "70%", margin: "0 auto", "--index" : "2" }}
         />
